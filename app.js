f114a012ab3906cbf29cec20a5a9da0ed8ff43b3
shahzodbekFrontend <shahzodbekrahmonjonov83@gmail.com>
2022-10-10 11:35:16 +0500

    nev site

---
+++ app.js
@@ -0,0 +1,20 @@
+const shop = document.getElementById("shop");
+const drobdv = document.getElementById("drobdv")
+const minus = document.getElementById("minus");
+const pulus = document.getElementById("pulus");
+const number = document.getElementById("number");
+const modal = document.querySelector(".madal");
+const none = document.querySelector('.none')
+let add_number = 1;
+minus.addEventListener("click", () => {
+  number.innerText = add_number++;
+});
+pulus.addEventListener("click", () => {
+    if (add_number > -1) {
+        number.innerText = add_number--;
+    }
+});
+
+drobdv.addEventListener("click", () => {
+    modal.classList.toggle("none")
+});
